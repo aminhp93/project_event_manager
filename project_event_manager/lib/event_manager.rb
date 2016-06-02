42e67559b2cf5f13dcf3367abdb6a8727ccdfeb5
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-02 01:25:28 -0700

    Moving Form Letter Generation to a Method

--- project_event_manager/lib/event_manager.rb
+++ project_event_manager/lib/event_manager.rb
@@ -14,6 +14,16 @@ def legislators_by_zipcode(zipcode)
 	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
+def save_thank_you_letters(id, form_letter)
+	Dir.mkdir("output") unless Dir.exists? "output"
+
+	filename = "output/thanks_#{id}.html"
+
+	File.open(filename,'w') do |file|
+		file.puts form_letter
+	end
+end
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 template_letter = File.read "form_letter.erb"
@@ -29,13 +39,7 @@ contents.each do |row|
 
 	form_letter = erb_template.result(binding)
 
-	Dir.mkdir("output") unless Dir.exists? "output"
-
-	filename = "output/thanks_#{id}.html"
-
-	File.open(filename,'w') do |file|
-		file.puts form_letter
-	end
+	save_thank_you_letters(id, form_letter)
 end
 
 
