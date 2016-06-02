c04cd947126f0e163d5f1250efdceaba4db16c5e
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-02 00:27:31 -0700

    Skipping the Header Row

--- project_event_manager/lib/event_manager.rb
+++ project_event_manager/lib/event_manager.rb
@@ -1,18 +1,17 @@
 puts "EventManager Initialized"
 
-# content = File.read "event_attendees.csv"
-# puts content
-
 # lines = File.readlines "event_attendees.csv"
 # lines.each do |line|
-# 	columns = lines.split(",")
-# 	puts line
+# 	next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+# 	columns = line.split(",")
+# 	name = columns[2]
+# 	puts name
 # end
 
-lines = File.readline "event_attendees.scv"
-lines.each do |line|
+lines = File.readlines "event_attendees.csv"
+lines.each_with_index do |line, index|
+	next if index == 0
 	columns = line.split(",")
 	name = columns[2]
-	# puts columns
 	puts name
 end
