b73b0ed221e3f86a0b6ea880e014d03b7113f3e7
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-02 00:38:39 -0700

    Handling Missing Zipcode

--- project_event_manager/lib/event_manager.rb
+++ project_event_manager/lib/event_manager.rb
@@ -7,12 +7,14 @@ contents.each do |row|
 	name = row[:first_name]
 	zipcode = row[:zipcode]
 
-	if zipcode.length < 5
+	if zipcode.nil?
+		zipcode = "00000"
+	elsif zipcode.length < 5
 		zipcode = zipcode.rjust(5, "0")
 	elsif zipcode.length > 5
 		zipcode = zipcode[0..4]
 	end
-	
+
 	puts "#{name} #{zipcode}"
 end
 
