4d2f7b43519695965f1d74b72069b8eab1f8e768
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-02 00:40:29 -0700

    Moving Clean Zip Codes to a Method

--- project_event_manager/lib/event_manager.rb
+++ project_event_manager/lib/event_manager.rb
@@ -2,11 +2,7 @@ require "csv"
 
 puts "EventManager Initialized"
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-contents.each do |row|
-	name = row[:first_name]
-	zipcode = row[:zipcode]
-
+def clean_zipcode(zipcode)
 	if zipcode.nil?
 		zipcode = "00000"
 	elsif zipcode.length < 5
@@ -14,6 +10,14 @@ contents.each do |row|
 	elsif zipcode.length > 5
 		zipcode = zipcode[0..4]
 	end
+end
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+	name = row[:first_name]
+	zipcode = row[:zipcode]
+
+	zipcdoe = clean_zipcode(row[:zipcode])
 
 	puts "#{name} #{zipcode}"
 end
