3667403470dc09d7954c4090386f9814cc2b23c5
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-02 00:30:09 -0700

    Switching over using the CSV library

--- project_event_manager/lib/event_manager.rb
+++ project_event_manager/lib/event_manager.rb
@@ -1,17 +1,11 @@
+require "csv"
+
 puts "EventManager Initialized"
 
-# lines = File.readlines "event_attendees.csv"
-# lines.each do |line|
-# 	next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-# 	columns = line.split(",")
-# 	name = columns[2]
-# 	puts name
-# end
-
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-	next if index == 0
-	columns = line.split(",")
-	name = columns[2]
+contents = CSV.open "event_attendees.csn", headers: true
+contents.each do |row|
+	name = row[2]
 	puts name
 end
+
+
