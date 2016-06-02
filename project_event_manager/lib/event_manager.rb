6c3cd9913f1f58e7d9c27b5e2164dabf371d1781
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-02 00:35:21 -0700

    Displaying the Zip Codes of All Attendees

--- project_event_manager/lib/event_manager.rb
+++ project_event_manager/lib/event_manager.rb
@@ -2,10 +2,11 @@ require "csv"
 
 puts "EventManager Initialized"
 
-contents = CSV.open "event_attendees.csv", headers: true
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
 	name = row[:first_name]
-	puts name
+	zipcode = row[:zipcode]
+	puts "#{name} #{zipcode}"
 end
 
 
