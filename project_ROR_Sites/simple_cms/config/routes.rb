66aee005ad3a3b27e394354e26e5e6b447743071
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-05 22:09:16 -0700

    Redirect action

--- project_ROR_Sites/simple_cms/config/routes.rb
+++ project_ROR_Sites/simple_cms/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root :to => "demo#index"
+  
+  # get 'demo/index'
+
+  # match 'demo/index', :to => 'demo#index', :via => :get
+
+  match ':controller(/:action(/:id(.:format)))', :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
